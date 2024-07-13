e088f377059bb4f1fd50c74111e6d42f79c444ee
WjatscheslawL <166384651+WjatscheslawL@users.noreply.github.com>
2024-07-13 13:12:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-TOKEN = "6391454574:AAGZ0OP85s5SVKgDnFy9PEP6k444gbGhokw"
 
-API_BITCOIN = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
-
-API_WETTER ='fd2b7a60664909ef6733adc332f7109f'
